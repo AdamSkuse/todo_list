ea79c9f287ac2d07b3986c6e1cd84d6867b79c13
AdamSkuse <a.t.skuse@gmail.com>
2016-05-31 17:19:56 +0100

    Completes Step 5

--- controllers.js
+++ controllers.js
@@ -1,8 +1,17 @@
 toDoApp.controller('TodoListController', function(){
+  // var self = this;
   this.todos = [
     {task: "ToDo1", done: true},
     {task: "Kill Cat", done: false}];
-     
+
+  this.addToDo = function(todoText){
+    this.todos.push({task: todoText, done: false});
+  };
+
+  this.removeToDo = function(){
+    this.todos.pop();
+  };
+  
 });
 
 
