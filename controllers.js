d40a94657336a406ae0a5bcb7532a5ddcd48bf55
AdamSkuse <a.t.skuse@gmail.com>
2016-05-31 14:36:51 +0100

    Adds controller

---
+++ controllers.js
@@ -0,0 +1,15 @@
+toDoApp.controller('TodoListController', function(){
+  this.todo = "ToDo1";
+});
+
+
+// {
+//     var todoList = this;
+//     todoList.todos = [
+//       {text:'learn angular', done:true},
+//       {text:'build an angular app', done:false}];
+//
+//     todoList.addTodo = function() {
+//       todoList.todos.push({text:todoList.todoText, done:false});
+//       todoList.todoText = '';
+//     };
