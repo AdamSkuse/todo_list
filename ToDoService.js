887746d97ffe5ebbb160525fdaa9937e4ef6b2dd
AdamSkuse <a.t.skuse@gmail.com>
2016-06-01 12:33:42 +0100

    refactor part 7

--- ToDoService.js
+++ ToDoService.js
@@ -2,13 +2,24 @@ toDoApp.service('ToDoService', ['$http', 'ToDoFactory', function($http, ToDoFact
 
   this.getAll = function() {
     var todos = []
-    $http.get('http://quiet-beach-24792.herokuapp.com/todos.json').then(function(response) {
-      response.data.forEach(function (item) {
-        todos.push(new ToDoFactory(item.text, item.completed));
-      });
-    });
+    _callAPI(todos)
     return todos
 
+  };
+
+  function _callAPI(todos){
+    $http.get('http://quiet-beach-24792.herokuapp.com/todos.json').then(function(response) {
+      return _loopyLoopDoop(response, todos)
+
+    });
+  };
+
+  function _loopyLoopDoop(response, todos){
+    response.data.forEach(function (item) {
+      todos.push(new ToDoFactory(item.text, item.completed));
+
+    });
+    return todos;
   };
 
 }]);
